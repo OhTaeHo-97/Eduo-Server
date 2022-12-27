b9b580809132a04f1fc673228188c5ca24c3a20c
장정민 <xvsgxv@gmail.com>
2022-12-27 15:40:28 +0900

    Commit 확인

--- Eduo/src/main/java/com/eduo/student/controller/StudentController.java
+++ Eduo/src/main/java/com/eduo/student/controller/StudentController.java
@@ -1,8 +1,12 @@
 package com.eduo.student.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
+import com.eduo.student.model.entity.Student;
 import com.eduo.student.model.service.StudentService;
 
 public class StudentController {
